71898fec40af12026688d36b89f68855059198fd
Luca Zanchetta <102416762+luca-zanchetta@users.noreply.github.com>
2023-08-09 12:29:51 +0200

    Login API starting point
    The login is NOT working; it is just a trial version for exchanging data between the frontend and the backend

--- Flask Server/server.py
+++ Flask Server/server.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def members():
-    
     return { "test" : "test"}
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    return jsonify({'username':username, 'password':password}), 200
 
 
 if __name__ == "__main__":
