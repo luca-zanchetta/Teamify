f02dc1dcfebdaff380cce596a94422ef6fa2310b
Luca Zanchetta <102416762+luca-zanchetta@users.noreply.github.com>
2023-08-13 18:11:44 +0200

    Alert fix and pop-up implementation

--- React-site/src/HomeLoggedIn.js
+++ React-site/src/HomeLoggedIn.js
@@ -3,21 +3,31 @@ import './Css/Homepage.css'
 
 import NavBar from './Components/NavBar';
 import TopBar from './Components/TopBar';
-import {Container} from './Css/Navigator.css'
 
 import {Link, useNavigate} from 'react-router-dom'
 import UserIcon from './Components/UserIcon';
-
+import Alert from "./Components/Alert.tsx";
 
 
 
 function HomeLoggedIn() {
-  const loggedIn = localStorage.getItem('user');
+  const loggedIn = localStorage.getItem('LoggedUser');
   const navigate = useNavigate();
 
   if(!loggedIn) {   // If the user is not logged in, (s)he has to login first.
     navigate('/login');
   }
+
+  /* ALERT SECTION */
+
+
+  const login_200 = sessionStorage.getItem("login_alert") === "true";
+  const handleLogin = () => {
+    sessionStorage.setItem("login_alert", "false");
+  };
+
+
+  /* END ALERT SECTION */
 
   return ( 
     <div className='App'>
@@ -34,7 +44,11 @@ function HomeLoggedIn() {
             <UserIcon></UserIcon>
           </div> 
       </div>
-    
+      {login_200 && (
+        <Alert onClick={handleLogin} state="success">
+          Welcome {loggedIn}!
+        </Alert>
+      )}
       <div className='SideContainer'>
         <NavBar></NavBar>
       </div>
