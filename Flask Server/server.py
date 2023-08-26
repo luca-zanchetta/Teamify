9f9f8848f57023c2e78f481399cceae089bebf57
Matteo Simonetti <anonimo345423@gmail.com>
2023-08-26 18:21:16 +0200

    Update server.py

--- Flask Server/server.py
+++ Flask Server/server.py
@@ -880,4 +880,4 @@ def reject_invite():
 
 if __name__ == "__main__":
     # app.run(debug=True, host="localhost", port=5000)
-    socketio.run(app, host="localhost", port=5000)
+    socketio.run(app, host="localhost", port=5000, debug=True)
