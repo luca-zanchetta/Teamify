d61669e36b6badf854e41bf3528ca5478db403ae
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2023-09-05 08:54:50 +0200

    Home page
    miss icons event/chat

--- React-site/src/Home.js
+++ React-site/src/Home.js
@@ -5,13 +5,18 @@ import { TopBar, BarHeading } from "./css/Login.css";
 import { Link, Navigate } from "react-router-dom";
 
 import calendar from "./icons/calendar.png";
-import paper from "./icons/paper.png";
-import profits from "./icons/profits.png";
-import savings from "./icons/savings.png";
+import team from "./icons/team.png";
+import profile from "./icons/users.png";
+import back from "./img/background_green.jpeg";
+import chat from "./icons/chat.png";
 
 const loggedIn = localStorage.getItem("LoggedUser");
 
 function Home() {
+  const containerStyle = {
+    background: `url(${back}) no-repeat center center fixed`,
+    backgroundSize: "cover",
+  };
   return (
     <div className="App">
       {loggedIn && <Navigate to="/home" />}
@@ -41,62 +46,64 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="CenterContainer">
-        <div className="TextContainer">
-          <h1>Lorem ipsum dolor sit amet</h1>
-          Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis
-          suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
-        </div>
+      <div
+        className="CenterContainer"
+        style={{ ...containerStyle, height: "100%" }}
+      >
+        <h1 style={{ marginTop: "6%" }}>Teamify</h1>
+        <h5 style={{ marginTop: "1%", marginBottom: "3%" }}>
+          Organize, Collaborate, and Achieve Team Goals
+        </h5>
         <div className="CardContainer">
           <div className="Card">
             <div className="CardIcon">
               <img src={calendar}></img>
             </div>
-            <div className="CardText">
-              <h4>Build and ship faster using simple tools</h4>
+            <div className="CardText" style={{ marginLeft: "3%" }}>
+              <h4>Intuitive Weekly Calendar</h4>
               <h5>
-                All of our products are built with simplicity at their core, so
-                you can spend your time focusing on building apps, not
-                infrastructure.
+                View all your work and events in a single, clear, and intuitive
+                view. The weekly calendar provides a comprehensive overview of
+                planned activities for you and your team.
               </h5>
             </div>
           </div>
           <div className="Card">
             <div className="CardIcon">
-              <img src={paper}></img>
+              <img src={team}></img>
             </div>
-            <div className="CardText">
-              <h4>Build and ship faster using simple tools</h4>
+            <div className="CardText" style={{ marginLeft: "3%" }}>
+              <h4>Collaborative Workgroups</h4>
               <h5>
-                All of our products are built with simplicity at their core, so
-                you can spend your time focusing on building apps, not
-                infrastructure.
+                Team creation is the process of assembling a group of
+                individuals with diverse skills, expertise, and roles to work
+                collectively towards a common goal or project.
               </h5>
             </div>
           </div>
           <div className="Card">
             <div className="CardIcon">
-              <img src={profits}></img>
+              <img src={profile}></img>
             </div>
-            <div className="CardText">
-              <h4>Build and ship faster using simple tools</h4>
+            <div className="CardText" style={{ marginLeft: "3%" }}>
+              <h4>Personal task and shared event</h4>
               <h5>
-                All of our products are built with simplicity at their core, so
-                you can spend your time focusing on building apps, not
-                infrastructure.
+                From managing personal and shared tasks with ease to organizing
+                collaborative events seamlessly, Teamify simplifies teamwork,
+                ensuring nothing gets overlooked.
               </h5>
             </div>
           </div>
           <div className="Card">
             <div className="CardIcon">
-              <img src={savings}></img>
+              <img src={chat}></img>
             </div>
-            <div className="CardText">
-              <h4>Build and ship faster using simple tools</h4>
+            <div className="CardText" style={{ marginLeft: "3%" }}>
+              <h4>Team communication</h4>
               <h5>
-                All of our products are built with simplicity at their core, so
-                you can spend your time focusing on building apps, not
-                infrastructure.
+                Versatile and user-friendly platform for team members to
+                communicate effectively. It fosters collaboration by enabling
+                real-time conversations and easy access to team discussions.
               </h5>
             </div>
           </div>
