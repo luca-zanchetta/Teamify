c8d5b72523f6b2ce82266ce1cee2ec0a61df230d
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2023-09-11 16:27:12 +0200

    Working deployment

--- Flask Server/DBConnection.py
+++ Flask Server/DBConnection.py
@@ -1,18 +1,22 @@
 import psycopg2
 
-user = "postgres"
-psw = "postgres"
-docker = "db"
-local = "localhost"
-
+user="postgres"
+psw="postgres"
+db="db"
+db_backup = "db-backup"
+local="localhost"
 
 def get_connection():
     try:
-        conn = psycopg2.connect(
-            host=local, port=5432, dbname="teamify", user=user, password=psw
-        )
+        print('[INFO] Trying on the primary...')
+        conn = psycopg2.connect(host=db, port=5432, dbname="postgres", user=user, password=psw)
         return conn
-
+    
     except:
-        print("Error during db connection\n")
-        return None
+        try:
+            print('[INFO] Trying on the backup...')
+            conn = psycopg2.connect(host=db_backup, port=5432, dbname="postgres", user=user, password=psw)
+            return conn
+        except:
+            print("Error during db connection\n")
+            return None
