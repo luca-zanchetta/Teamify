3418583c1bb3d574675fd8fd3a6a2062bba8a4ca
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2023-09-12 15:20:55 +0200

    Fix primary-backup

--- Flask Server/DBConnection.py
+++ Flask Server/DBConnection.py
@@ -7,17 +7,12 @@ psw="postgres"
 hosts = ["db", "db-backup"]
 
 def get_connection():
-    chosen_host = random.choice(hosts)
-    other_host = hosts[1] if chosen_host == hosts[0] else hosts[0]
-
     try:
-        print(f'[INFO] {chosen_host}')
-        conn = psycopg2.connect(host=chosen_host, port=5432, dbname="postgres", user=user, password=psw, connect_timeout=3)
+        conn = psycopg2.connect(host=hosts[0], port=5432, dbname="postgres", user=user, password=psw, connect_timeout=3)
         return conn
     except:
         try:
-            print(f'[INFO] {other_host}')
-            conn = psycopg2.connect(host=other_host, port=5432, dbname="postgres", user=user, password=psw, connect_timeout=3)
+            conn = psycopg2.connect(host=hosts[1], port=5432, dbname="postgres", user=user, password=psw, connect_timeout=3)
             return conn
         except:
             print("Error during db connection\n")
