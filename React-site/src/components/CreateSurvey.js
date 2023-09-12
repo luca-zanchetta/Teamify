53a59e3dca454fb121fc985c281f36f7427dedcf
Matteo Simonetti <anonimo345423@gmail.com>
2023-09-12 13:33:45 +0200

    user can no longer create surveys (frontend)

--- React-site/src/components/CreateSurvey.js
+++ React-site/src/components/CreateSurvey.js
@@ -12,6 +12,8 @@ import { address, flask_port } from "./Endpoint";
 function CreateSurvey() {
 
   const [show, setShow] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [testAdmin, setTestAdmin] = useState(false);
   const [data, setData] = useState(false);
   const [options,setOptions] = useState(1)
   const queryParameters = new URLSearchParams(window.location.search);
@@ -19,6 +21,8 @@ function CreateSurvey() {
   const username = localStorage.getItem("LoggedUser");
   const decryptedUsername = localStorage.getItem("username");
   const endpointCreate= address + flask_port + "/createPool";
+  const endpointIsAdmin= address + flask_port + "/teamDetails";
+  
 
   function AddOption() {
     setOptions(options+1);
@@ -79,67 +83,92 @@ function CreateSurvey() {
     }
 }
 
-    useEffect(() => {
+    if(testAdmin===false){
+      try {
+        // Send a GET request to the endpoint of the Flask server
+        const response1 = axios
+          .get(endpointIsAdmin, {
+            params: {
+              id: id
+            }
+          }).then((response) => {
+            var admins=response.data[0]["admins"]
+            if(admins.includes(decryptedUsername)) setIsAdmin(true);
+            console.log(isAdmin);
 
-      
-      
-    }, []);
-
-  return (
-    <div className="SurveySectionTopBar">
-    {
-      !show &&
-      (
-        <div className="CreateSurvey" onClick={ToggleCreate}>
-          <h2>
-            Add Survey
-          </h2>           
-        </div>
-        )
-        ||
-        show && 
-        (
-        <div className="SurveyContainer">
-          <div className="SurveyHeader">
-            <h2>Create New Survey</h2>
-          </div>
-          <div className="SurveyHeader">
-            <input type="text" placeholder ="insert title here" id="title"></input>
-            <input type="date" placeholder ="insert title here" id="date"></input>
-          </div>
-        <div className="SurveyBody">
-        {
-          [...Array(options)].map((a,i) => (
-            <div className="SurveyEntry" style={{justifyContent:"spaceAround"}}>
-              <span class="dot"></span>
-              <input type="text" placeholder ="insert option here" className="option" id={i}></input>
-              <div className="XIcon" onClick={RemoveOptions}> <img src={cancel} class={i}></img></div>
-            </div> 
-          ))
-        }
-          <div className="SurveyEntry" style={{justifyContent:"spaceAround"}}>
-            <span class="dot"></span>
-            <input type="text" placeholder ="Add Another option" className="option" onFocus={(e) => e.target.blur()}onClick={AddOption} readOnly={true}></input>
-          </div> 
-        </div>
-
-        <div className="SurveyFooter" style={{justifyContent: "flex-end"}}>
-          <div className="CreateSurvey" onClick={ToggleCreate}>
-            <h2 style={{color:"white"}}>
-              cancel
-            </h2>           
-          </div>
-          <div className="CreateSurvey" style={{ marginLeft:"1%"}} onClick={Confirm}>
-            <h2 style={{color:"white"}}>
-              confirm
-            </h2>           
-          </div>
-        </div>
-      </div>
-      )
+          }).catch(function (error) {
+            if (error.response) {
+              // Print error data
+              console.log("Data: " + error.response.data);
+              console.log("Status: " + error.response.status);
+              console.log("Headers: " + error.response.headers);
+            }
+          });
+      } catch (error) {
+        // Request failed
+        console.log("[ERROR] Request failed: " + error);
+      }
+      setTestAdmin(true);
     }
-    </div>
-  );
+
+    return (
+      isAdmin && (
+        <div className="SurveySectionTopBar">
+        {
+          !show &&
+          (
+            <div className="CreateSurvey" onClick={ToggleCreate}>
+              <h2>
+                Add Survey
+              </h2>           
+            </div>
+            )
+            ||
+            show && 
+            (
+            <div className="SurveyContainer">
+              <div className="SurveyHeader">
+                <h2>Create New Survey</h2>
+              </div>
+              <div className="SurveyHeader">
+                <input type="text" placeholder ="insert title here" id="title"></input>
+                <input type="date" placeholder ="insert title here" id="date"></input>
+              </div>
+            <div className="SurveyBody">
+            {
+              [...Array(options)].map((a,i) => (
+                <div className="SurveyEntry" style={{justifyContent:"spaceAround"}}>
+                  <span class="dot"></span>
+                  <input type="text" placeholder ="insert option here" className="option" id={i}></input>
+                  <div className="XIcon" onClick={RemoveOptions}> <img src={cancel} class={i}></img></div>
+                </div> 
+              ))
+            }
+              <div className="SurveyEntry" style={{justifyContent:"spaceAround"}}>
+                <span class="dot"></span>
+                <input type="text" placeholder ="Add Another option" className="option" onFocus={(e) => e.target.blur()}onClick={AddOption} readOnly={true}></input>
+              </div> 
+            </div>
+    
+            <div className="SurveyFooter" style={{justifyContent: "flex-end"}}>
+              <div className="CreateSurvey" onClick={ToggleCreate}>
+                <h2 style={{color:"white"}}>
+                  cancel
+                </h2>           
+              </div>
+              <div className="CreateSurvey" style={{ marginLeft:"1%"}} onClick={Confirm}>
+                <h2 style={{color:"white"}}>
+                  confirm
+                </h2>           
+              </div>
+            </div>
+          </div>
+          )
+        }
+        </div>
+      )
+    );
+    
   
 }
 
